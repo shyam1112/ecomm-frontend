11729c8a9bf4ca2a6c7cbc472ab3963320e12d0f
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-08-29 19:34:50 +0530

    a

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,30 @@
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
-function Nav(){
-    const auth=localStorage.getItem('user');
-    const navigate=useNavigate();
-    const logout=()=>{
+function Nav() {
+    const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout = () => {
         localStorage.clear();
         navigate('/signup');
     }
-    return(
+    return (
         <div>
+            <img alt="logo" className="logo" src="https://scontent.fbom19-3.fna.fbcdn.net/v/t39.30808-6/300794579_421759580020244_753011159807902456_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=HjPvBkNODnUAX_-mvYC&_nc_ht=scontent.fbom19-3.fna&oh=00_AfAkkvzz5AhPZD4CQYY2G1znUJ5tJO8xeew-qwb5AUpEow&oe=64F28D69"/>
             <div className="navdiv">
-            <ul className="nav-li">
-                <li className="item"><Link to='/'>Home</Link></li>
-                <li className="item"><Link to='/add'>Add product</Link></li>
-                <li className="item"><Link to='/update'>Update product</Link></li>
-                <li className="item"><Link to='/Profile'>Profile</Link></li>
-                <li className="item" >{auth?<Link onClick={logout} to='/signup'>Logout</Link>:
-                <div>
-                <Link to='/signup'>Sign-up</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-                <Link to='/login'>Login</Link>
+                {
+                    auth?<ul className="nav-li">
+                        <li className="item"><Link to='/'>Home</Link></li>
+                        <li className="item"><Link to='/add'>Add product</Link></li>
+                        <li className="item"><Link to='/update'>Update product</Link></li>
+                        <li className="item"><Link to='/Profile'>Profile</Link></li>
+                        <li className="item" > <Link onClick={logout} to='/signup'>Logout ({JSON.parse(auth).name})</Link> </li>
+                    </ul>:
+                    <div className="nav-li navright">
+                    <Link to='/signup'>Sign-up</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <Link to='/login'>Login</Link>
                 </div>
-                }</li>
-                
-            </ul>
+
+                }
             </div>
         </div>
     )
