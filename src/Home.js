aec79403d494c288f68ce43963da683598da8e33
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-09-04 18:49:01 +0530

    a

--- src/Home.js
+++ src/Home.js
@@ -1,53 +1,69 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function Home(){
-    const [product,setProduct]=useState();
+function Home() {
+    const [product, setProduct] = useState();
 
-    useEffect(()=>{
+    useEffect(() => {
         getData();
-    },[]);
+    }, []);
 
-    const getData=async()=>{
-        let result=await fetch("http://localhost:5000/products");
-        result=await result.json();
+    const getData = async () => {
+        let result = await fetch("http://localhost:5000/products");
+        result = await result.json();
         setProduct(result);
     }
 
-    const deleteitem= async(id)=>{
-        let result=await fetch(`http://localhost:5000/products/${id}`,{
-          method:"delete"  
+    const deleteitem = async (id) => {
+        let result = await fetch(`http://localhost:5000/products/${id}`, {
+            method: "delete"
         });
-        result=await result.json();
-        if(result){
+        result = await result.json();
+        if (result) {
             alert("Product deleted successfully.");
             getData();
         }
-        
+
     }
-    return(
+    const searchdata = async (event) => {
+        let key = event.target.value;
+        if (key) {
+
+            let result = await fetch(`http://localhost:5000/search/${key}`);
+            result=await result.json();
+            if (result) {
+                setProduct(result);
+            }
+        } else {
+            getData();
+        }
+    }
+    return (
         <div className="product">
             <h2>Product List</h2>
+            <input type="text" className="search" placeholder="Search the product" onChange={searchdata} />
             <ul>
                 <li><strong>S. No</strong></li>
                 <li><strong>Name</strong></li>
                 <li><strong>Price</strong></li>
                 <li><strong>Category</strong></li>
                 <li><strong>Company</strong></li>
-                <li style={{width:300}}><strong>Operation</strong></li>
+                <li style={{ width: 300 }}><strong>Operation</strong></li>
             </ul>
             {
-                product?.map((item,index)=>
+                product.length>0 ?product?.map((item, index) =>
                     <ul key={item._id}>
-                        <li>{index+1}</li>
+                        <li>{index + 1}</li>
                         <li>{item.name}</li>
                         <li>{item.price}</li>
                         <li>{item.category}</li>
                         <li>{item.company}</li>
-                        <li className="operation"><button style={{}} onClick={()=>deleteitem(item._id)}>Delete</button></li>
-                        <li className="operation"><Link to={"/update/"+item._id}>Update</Link></li>
+                        <li className="operation"><button style={{}} onClick={() => deleteitem(item._id)}>Delete</button></li>
+                        <li className="operation"><Link to={"/update/" + item._id}>Update</Link></li>
                     </ul>
                 )
+                :
+                <h1>No result found.</h1>
             }
         </div>
     )
